08255066e85dd50cce5df36cb70096f5778a8dd7
leandroquiroga <leandroquiroga9514@gmail.com>
2023-03-15 09:40:17 -0300

    formik: getFieldProps

--- src/forms/pages/FormikYupPage.tsx
+++ src/forms/pages/FormikYupPage.tsx
@@ -1,42 +1,43 @@
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import "../styles/styles.css";
-import { validationErrorSchema } from '../helpers';
-import { valueFormWithYup } from '../helpers/index';
+import { validationErrorSchema } from "../helpers";
+import { valueFormWithYup } from "../helpers/index";
 
 export const FormikYupPage = () => {
-
-  const {handleSubmit, handleChange, handleReset ,values, errors, touched, handleBlur} = useFormik({
+  const {
+    handleSubmit,
+    handleChange,
+    handleReset,
+    values,
+    errors,
+    touched,
+    handleBlur,
+    getFieldProps
+  } = useFormik({
     initialValues: valueFormWithYup,
-    onSubmit: values => {
-      console.log(values)
+    onSubmit: (values) => {
+      console.log(values);
     },
 
-    validationSchema: Yup.object(validationErrorSchema)
-  })
+    validationSchema: Yup.object(validationErrorSchema),
+  });
   return (
     <div>
       <h1> Formik Yup</h1>
       <form noValidate onSubmit={handleSubmit}>
-        <label
-          htmlFor="firstName">
-            First Name:
-        </label>
+        <label htmlFor="firstName">First Name:</label>
         <input
           type="text"
-          name="firstName"
           id="firstName"
-          placeholder="First Name"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.firstName}
+          placeholder="First Name"  
+          {...getFieldProps('firstName')}
         />
-        { touched.firstName && errors.firstName && <span>{ errors.firstName }</span>}
-        <label 
-          htmlFor="lastName">
-          Last Name:
-        </label>
+        {touched.firstName && errors.firstName && (
+          <span>{errors.firstName}</span>
+        )}
+        <label htmlFor="lastName">Last Name:</label>
         <input
           type="text"
           name="lastName"
@@ -46,12 +47,9 @@ export const FormikYupPage = () => {
           onBlur={handleBlur}
           value={values.lastName}
         />
-        { touched.lastName && errors.lastName && <span>{ errors.lastName }</span>}
+        {touched.lastName && errors.lastName && <span>{errors.lastName}</span>}
 
-        <label
-          htmlFor="email">
-          Email:
-        </label>
+        <label htmlFor="email">Email:</label>
         <input
           type="email"
           name="email"
@@ -61,12 +59,11 @@ export const FormikYupPage = () => {
           onBlur={handleBlur}
           value={values.email}
         />
-        { touched.email && errors.email && <span>{ errors.email }</span>}
+        {touched.email && errors.email && <span>{errors.email}</span>}
         <button type="submit">Send</button>
-        <button
-          type="reset"
-          onClick={handleReset}
-        >Reset</button>
+        <button type="reset" onClick={handleReset}>
+          Reset
+        </button>
       </form>
     </div>
   );
